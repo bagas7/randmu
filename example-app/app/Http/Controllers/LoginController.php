6028a8ed97f09350d2248ecb8b5788c60dd28d3b
bagas7 <bagas.prakoso3108@gmail.com>
2022-11-21 11:38:28 +0700

    proses perbaikan halaman login 2

---
+++ example-app/app/Http/Controllers/LoginController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('login', [
+            'title' => 'login',
+            'css' => 'css/body.css',
+            'css2' => '',
+            'js' => '',
+            'ckeditor' => ''
+        ]);
+    }
+
+    public function authenticate(Request $request){
+
+        $credentials = $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        if(Auth::attempt($credentials)){
+            //regenerate untuk membuat session baru
+            $request->session()->regenerate();
+            //intended adalah mengalihkan halaman ketika sudah melewati middleware
+            //middleware untuk mengechek user sudah login atau belum. jika sudah maka akan diarahkan di route yg kita inginkan
+            //untuk mengatur route bisa di app/providers/RouteServiceProvider.php
+
+            return redirect()->intended('/dashboard');
+        }
+        //back untuk mengembalikan ke halaman sebelumnya
+        //with untuk memberi flash session dengan nama sessionnya pada parameter pertama dan parameter kedua untuk isi session
+        //flash session adalah session yang hanya ada pada satu halaman saja
+        return back()->with('errorLogin', 'email dan password tidak sesuai');
+    }
+
+    public function logout(Request $request){
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
+}
