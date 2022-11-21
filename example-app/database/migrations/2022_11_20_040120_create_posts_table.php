44be52d4d0ac08b43dc4857ec60b0dcafa2c4f7b
bagas7 <bagas.prakoso3108@gmail.com>
2022-11-21 10:02:29 +0700

    MVC

---
+++ example-app/database/migrations/2022_11_20_040120_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('title');
+            $table->longText('content');
+            $table->longText('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
